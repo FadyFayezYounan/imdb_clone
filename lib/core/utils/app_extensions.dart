66aa0826d1a30349dcb078343e253e75a23aae1f
FadyFayezYounan <fadyfayez067@gmail.com>
2023-11-17 13:38:13 +0200

    update app extensions

--- lib/core/utils/app_extensions.dart
+++ lib/core/utils/app_extensions.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/cupertino.dart';
 
 extension DeviceInfo on BuildContext {
-  bool get isPortrait =>
-      MediaQuery.of(this).orientation == Orientation.portrait;
-  double get height => MediaQuery.of(this).size.height;
-  double get width => MediaQuery.of(this).size.width;
+  bool get isPortrait => MediaQuery.orientationOf(this) == Orientation.portrait;
+  double get height => MediaQuery.sizeOf(this).height;
+  double get width => MediaQuery.sizeOf(this).width;
 }
